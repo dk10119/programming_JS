2ef79a0dcee18e07da73a7d56ae24595a70a5aed
dk10119 <tranduclong092@gmail.com>
2024-01-22 13:18:27 +0200

    task 10 done and add comment

--- 02_week/strings/strings_10.js
+++ 02_week/strings/strings_10.js
@@ -4,9 +4,13 @@ Refine the capitalize function to ensure it capitalizes the first letter of the 
 
 function capitalize(word) {
   let wordLowerCase = word.toLowerCase();
+  //convert word to lower case
   let initialUpperCase = word[0].toUpperCase();
+  //cut the first character out and convert it to upper case
   let wordNoInitial = wordLowerCase.substring(1);
+  //cut the string out without the initial
   return initialUpperCase + wordNoInitial;
+  //return the upper case initial with the rest of the character in lower case
 }
 
 // Sample usage - do not modify
