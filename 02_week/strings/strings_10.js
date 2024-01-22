302a1adb5cac9f382bcc348bdc9224d6c3d68a03
dk10119 <tranduclong092@gmail.com>
2024-01-22 13:16:19 +0200

    task 10 done

--- 02_week/strings/strings_10.js
+++ 02_week/strings/strings_10.js
@@ -2,9 +2,14 @@
 Refine the capitalize function to ensure it capitalizes the first letter of the name parameter provided to it.
  */
 
-function capitalize(word) {}
+function capitalize(word) {
+  let wordLowerCase = word.toLowerCase();
+  let initialUpperCase = word[0].toUpperCase();
+  let wordNoInitial = wordLowerCase.substring(1);
+  return initialUpperCase + wordNoInitial;
+}
 
 // Sample usage - do not modify
-console.log(capitalize('sam')); // "Sam"
-console.log(capitalize('ALEX')); // "Alex"
-console.log(capitalize('chARLie')); // "Charlie"
+console.log(capitalize("sam")); // "Sam"
+console.log(capitalize("ALEX")); // "Alex"
+console.log(capitalize("chARLie")); // "Charlie"
