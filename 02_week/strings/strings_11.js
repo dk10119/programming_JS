0f7c8d2f0d1f3910e9da3b40bf66a8bd453686aa
dk10119 <tranduclong092@gmail.com>
2024-01-22 13:44:37 +0200

    extra task 11 done

---
+++ 02_week/strings/strings_11.js
@@ -0,0 +1,9 @@
+/**
+Go to the game room. Count how many pc and moniters it is and return it.
+ */
+let pcNumber = 1;
+let monitorsNumber = 2;
+console.log(`Number of PC is ${pcNumber}
+Number of monitors is ${monitorsNumber}`);
+
+// Thank you Margit for the physical exercise
