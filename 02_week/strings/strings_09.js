f4f060505940c85075f4bd4baf83208b67dd2571
dk10119 <tranduclong092@gmail.com>
2024-01-22 13:11:02 +0200

    task 9 done

--- 02_week/strings/strings_09.js
+++ 02_week/strings/strings_09.js
@@ -2,8 +2,10 @@
 Refine the getFullName function so that it returns the person's full name by utilizing string interpolation.
  */
 
-function getFullName(firstName, lastName) {}
+function getFullName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
 
 // Sample usage - do not modify
-console.log(getFullName('Sam', 'Doe')); // "Sam Doe"
-console.log(getFullName('Alex', 'Blue')); // "Alex Blue"
+console.log(getFullName("Sam", "Doe")); // "Sam Doe"
+console.log(getFullName("Alex", "Blue")); // "Alex Blue"
