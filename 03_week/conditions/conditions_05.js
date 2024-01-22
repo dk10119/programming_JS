50cabad14403be56aa6c0c4f1490a89894956ae3
dk10119 <tranduclong092@gmail.com>
2024-01-22 16:01:21 +0200

    condition task 5 completed

--- 03_week/conditions/conditions_05.js
+++ 03_week/conditions/conditions_05.js
@@ -2,9 +2,15 @@
 Complete the function evenOrOdd such that it returns the string "even" when the number parameter it receives is even and "odd" otherwise.
  */
 
-function evenOrOdd(number) {}
+function evenOrOdd(number) {
+  if (number % 2) {
+    return "odd";
+  } else {
+    return "even";
+  }
+}
 
 // Sample usage - do not modify
-console.log(evenOrOdd(25)); // true
-console.log(evenOrOdd(18)); // true
-console.log(evenOrOdd(-17)); // false
+console.log(evenOrOdd(25)); // "odd"
+console.log(evenOrOdd(18)); // "even"
+console.log(evenOrOdd(-17)); // "odd"
