21c799bdc7860c1589c3564ac514553c33d1a5ac
dk10119 <tranduclong092@gmail.com>
2024-01-27 19:09:46 +0200

    condition task 4 completed

--- 03_week/loops/loops_04.js
+++ 03_week/loops/loops_04.js
@@ -1,1 +1,28 @@
 // Develop a program that requests the user to input 20 numbers. After all numbers are entered, the program should display how many of these numbers are even.
+
+let nNaN = 0;
+let nOdd = 0;
+let nEven = 0;
+for (let i = 0; i < 20; i++) {
+  let n = prompt("Input a number. " + (20 - i) + " number(s) left.");
+  switch (true) {
+    case n % 2 == 1:
+      nOdd++;
+      break;
+    case n % 2 == 0:
+      nEven++;
+      break;
+    default:
+      nNaN++;
+  }
+}
+
+alert(
+  "Out of 20 inputs are " +
+    nEven +
+    " even, " +
+    nOdd +
+    " odd, " +
+    nNaN +
+    " not a number."
+);
