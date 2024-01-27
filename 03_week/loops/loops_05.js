009ed7e8756e4ff6177844a35dd4da854a29fb42
dk10119 <tranduclong092@gmail.com>
2024-01-27 19:39:09 +0200

    loop task 5 completed

--- 03_week/loops/loops_05.js
+++ 03_week/loops/loops_05.js
@@ -1,1 +1,32 @@
 // Develop a program that continuously prompts the user to input numbers until the user enters 0, at which point the program terminates. After termination, the program should calculate and display the average of all the entered numbers.
+
+let sum = 0;
+let count = 0;
+let num;
+let nNaN = 0;
+let string = "";
+while (num != 0) {
+  num = Number(
+    prompt("Input a number. Enter 0 to end the program and return the average.")
+  );
+  if (isNaN(num)) {
+    nNaN++;
+  } else {
+    sum += num;
+    count++;
+  }
+}
+let average = (sum / count).toFixed(4);
+if (nNaN == 1) {
+  string = " There is one input that is not a number.";
+} else if (nNaN > 1) {
+  string = " There is " + nNaN + " inputs that are not a number.";
+}
+alert(
+  "The arithmetic average of the " +
+    count +
+    " number input(s) are: " +
+    average +
+    "." +
+    string
+);
