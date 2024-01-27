672199ac30ec9eae4bc686e26e67e46ad64bbab8
dk10119 <tranduclong092@gmail.com>
2024-01-27 18:30:05 +0200

    condition task 3 completed

--- 03_week/loops/loops_03.js
+++ 03_week/loops/loops_03.js
@@ -1,7 +1,37 @@
 // Create a program that continuously prompts the user to input distance (in kilometers) and time (in hours) and then calculates the average speed. The program should terminate when the user enters 0 for the distance. Upon receiving a distance of 0, the program should not prompt for any further input.
 
-do {
-  let d = prompt("Enter the distant. (Enter 0 to end the program)");
-  let t = prompt("Enter the time.");
-  alert("Average speed is " + d / t);
-} while (d !== 0);
+/**
+let d;
+while (d != 0) {
+  d = prompt("Enter the distant. (Enter 0 to end the program)");
+  if (d == 0) {
+    break;
+  } else {
+    let t = prompt("Enter the time.");
+    if (d >= 0 || t > 0) {
+      alert("Average speed is " + d / t);
+    } else {
+    
+    }
+  }
+}
+ */
+
+let d;
+let t;
+while (d != 0) {
+  d = prompt("Enter the distant. (Enter 0 to end the program");
+  if (d == 0) {
+    break;
+  } else if (d < 0 || isNaN(d)) {
+    alert("Invalid input of distant value. Try again!");
+  } else {
+    let t = prompt("Enter the time.");
+    if (t <= 0 || isNaN(t)) {
+      alert("Invalid input of time value. Try again!");
+    } else {
+      v = (d / t).toFixed(4);
+      alert("Average velocity is: " + v);
+    }
+  }
+}
