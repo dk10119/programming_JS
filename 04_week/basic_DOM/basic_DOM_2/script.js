f79c94e55bc0037801befed9194999f4f03688f3
dk10119 <tranduclong092@gmail.com>
2024-02-02 13:02:28 +0200

    DOM task 2 added

--- 04_week/basic_DOM/basic_DOM_2/script.js
+++ 04_week/basic_DOM/basic_DOM_2/script.js
@@ -10,3 +10,22 @@ When the button is clicked, the function should:
 Ensure the input field is cleared after adding the fruit to the list.
 
 */
+const button = document.querySelector("#addFruitBtn"); //get the button
+
+function clearText() {
+  document.querySelector("#fruitInput").value = "";
+} //function to clear the input field
+
+function getInput() {
+  let fruitInput = document.querySelector("#fruitInput").value; //get the input
+
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(fruitInput)); //create the new <li> element with the input text
+
+  let fruitList = document.querySelector("#fruitList"); //get the <ul> list
+
+  fruitList.appendChild(li); //add the new <li> elemenet to the <ul> list
+  clearText();
+}
+
+button.addEventListener("click", getInput); // button activation
